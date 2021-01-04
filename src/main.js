d27d5e02d3e95127b95669bbe9d60a3ec715fc59
Kaid Mossburgh <kdmossbu@ksu.edu>
2021-01-04 11:52:41 -0600

    Create mobile demo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-$(document).ready(function() {
+let $menu = document.querySelector('#main-menu');
+let $navbar = document.querySelector("#main-navbar");
+let $menuToggle = document.querySelector('#toggle-navbar');
+let $portalMenu = document.querySelector('#portals-submenu');
 
-    $(document).click(function() {
-        console.log("1");
-    });
+$menuToggle.addEventListener('click', function() {
+
+    let $icon = $menuToggle.querySelector('i');
+    switchIcon($icon, 'fas fa-bars', 'fas fa-times');
+    $menu.classList.toggle('active');
 });
+
+$portalMenu.addEventListener('click', function() {
+
+    let $icon = $portalMenu.querySelector('i');
+    switchIcon($icon, 'fas fa-caret-down', 'fas fa-caret-up');
+    $portalMenu.classList.toggle('active');
+});
+
+function switchIcon($icon, active, inactive) {
+
+    if ($icon.className == active) $icon.className = inactive;
+    else $icon.className = active;
+}
